3734f0c0dd512cf169ef43efe8f6daf03c78caf7
Nattawut Phetmak <neizod@gmail.com>
2011-08-30 14:20:19 +0700

    [solved] gridland

---
+++ acm/set00/gridland.py
@@ -0,0 +1,19 @@
+import re
+import math
+
+raw = input()
+scene = int(raw)
+
+for i in range(scene):
+    raw = input()
+    raw = re.split('\W+', raw)
+    size = int(raw[0]) * int(raw[1])
+
+    ## this is mathematics approch, try it yourself and observe! ##
+    if size%2 == 10:
+        size = size - 1 + math.sqrt(2)
+
+    ## print the answer ##
+    print('Scenario #{}'.format(i+1))
+    print('{:.2f}'.format(size))
+    print()
