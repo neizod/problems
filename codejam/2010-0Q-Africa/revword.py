4cac904f984d2d33a1778bb9c7e50ebc836eed24
Nattawut Phetmak <neizod@gmail.com>
2012-02-21 03:28:36 +0700

    fix bug in one-line version

--- codejam/2010-0Q-Africa/revword.py
+++ codejam/2010-0Q-Africa/revword.py
@@ -1,5 +1,5 @@
 ﻿# one line version:
-# print('\n'.join('Case #{}: {}'.format(test+1, ' '.join(word for word in input().split()[::-1]) for test in range(int(input())))))
+# print('\n'.join('Case #{}: {}'.format(test+1, ' '.join(word for word in input().split()[::-1])) for test in range(int(input()))))
 
 for test in range(int(input())):
     revword = ' '.join(word for word in input().split()[::-1])
