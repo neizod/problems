9381f09c4bb690a1e12299cfcfaa029f0f752f32
Nattawut Phetmak <neizod@gmail.com>
2012-05-06 02:20:16 +0700

    add one-liner version

--- codejam/2008-1A/minscalar.py
+++ codejam/2008-1A/minscalar.py
@@ -1,9 +1,15 @@
-﻿for test in range(int(input())):
+# one-liner version
+"\n".join("Case #{0}: {2}".format(test+1, input(), sum(a*b for a, b in zip(sorted(int(val) for val in input().split()), sorted(int(val) for val in input().split())[::-1]))) for test in range(int(input())))
+
+###############################################################################
+
+
+for test in range(int(input())):
     size = int(input())
 
-    a = sorted([int(val) for val in input().split()])
-    b = sorted([int(val) for val in input().split()], reverse=True)
+    a = sorted(int(val) for val in input().split())
+    b = sorted(int(val) for val in input().split())[::-1]
 
-    scalar = sum([a[i]*b[i] for i in range(size)])
+    scalar = sum(a[i]*b[i] for i in range(size))
 
     print("Case #{}: {}".format(test+1, scalar))
