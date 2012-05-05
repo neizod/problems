6041d250ed8dd21400132a10011e9eccefd2d2c2
Nattawut Phetmak <neizod@gmail.com>
2012-05-06 02:35:47 +0700

    Update 2010-0Q-Africa/revword.py

--- codejam/2010-0Q-Africa/revword.py
+++ codejam/2010-0Q-Africa/revword.py
@@ -1,5 +1,5 @@
 ﻿# one-liner version
-'\n'.join('Case #{}: {}'.format(test+1, ' '.join(word for word in input().split()[::-1])) for test in range(int(input()))))
+'\n'.join('Case #{}: {}'.format(test+1, ' '.join(word for word in input().split()[::-1])) for test in range(int(input())))
 
 ###############################################################################
 
