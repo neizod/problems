7942c0731f94bdbbd898759eb4bbe91879d57c2b
Nattawut Phetmak <neizod@gmail.com>
2012-05-09 18:20:07 +0700

    test

--- codejam/2008-1A/minscalar.py
+++ codejam/2008-1A/minscalar.py
@@ -1,9 +1,9 @@
 ﻿for test in range(int(input())):
     size = int(input())
 
-    a = sorted([int(val) for val in input().split()])
-    b = sorted([int(val) for val in input().split()], reverse=True)
+    a = sorted(int(val) for val in input().split())
+    b = sorted(int(val) for val in input().split())[::-1]
 
-    scalar = sum([a[i]*b[i] for i in range(size)])
+    scalar = sum(a[i]*b[i] for i in range(size))
 
     print("Case #{}: {}".format(test+1, scalar))
