92aadbd725f4d07a7bb4761819a043c415962614
Nattawut Phetmak <neizod@gmail.com>
2012-08-22 02:43:37 +0700

    add thai's acm

---
+++ acm/thai/2012/g.py
@@ -0,0 +1,14 @@
+for test in range(int(input())):
+    input()
+    foods = [int(food) for food in input().split()]
+
+    day = 0
+    while all(foods):
+        foods.sort(reverse=True)
+        frig = foods.pop()
+        foods = [food-1 for food in foods]
+        foods.append(frig)
+        day += 1
+
+    print(day)
+
