fd771702f9240d25970c027d315fa4633902bd57
Nattawut Phetmak <neizod@gmail.com>
2018-10-19 02:48:03 +0700

    tabs -> spaces

--- acm/uva/11172-relational-operator.py
+++ acm/uva/11172-relational-operator.py
@@ -2,11 +2,11 @@
 
 
 def main():
-	for _ in range(int(input())):
-		n, m = [int(n) for n in input().split()]
-		ans = '<' if n < m else '>' if n > m else '='
-		print(ans)
+    for _ in range(int(input())):
+        n, m = [int(n) for n in input().split()]
+        ans = '<' if n < m else '>' if n > m else '='
+        print(ans)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- acm/uva/11727-cost-cutting.py
+++ acm/uva/11727-cost-cutting.py
@@ -2,10 +2,10 @@
 
 
 def main():
-	for i in range(int(input())):
-		ls = sorted(int(n) for n in input().split())
-		print(f'Case {i+1}: {ls[1]}')
+    for i in range(int(input())):
+        ls = sorted(int(n) for n in input().split())
+        print(f'Case {i+1}: {ls[1]}')
 
 
 if __name__ == '__main__':
-	main()
+    main()
